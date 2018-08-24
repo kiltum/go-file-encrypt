d5e11dc5b4626dae600e8cf65e7e92d9dc8cad38
Viacheslav Kaloshin <kiltum@kiltum.tech>
2018-08-24 13:26:11 +0300

    First Commit for test

---
+++ main.go
@@ -0,0 +1,85 @@
+package go_file_encrypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha256"
+	"golang.org/x/crypto/pbkdf2"
+	"io"
+	"os"
+)
+
+func deriveKey(passphrase string, salt []byte) ([]byte, []byte) {
+	if salt == nil {
+		salt = make([]byte, 8)
+		// http://www.ietf.org/rfc/rfc2898.txt
+		rand.Read(salt)
+	}
+	return pbkdf2.Key([]byte(passphrase), salt, 1000, 32, sha256.New), salt
+}
+
+// encrypt file on pat by password and aes256
+func encryptFile(path, password string) error {
+	inFile, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	salt := []byte(password) // I know this not good, but i haven't storage for salt
+	key, _ := deriveKey(password, salt)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+	var iv [aes.BlockSize]byte
+	stream := cipher.NewOFB(block, iv[:])
+
+	outFile, err := os.OpenFile(path+".encrypt", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	writer := &cipher.StreamWriter{S: stream, W: outFile}
+	// Copy the input file to the output file, encrypting as we go.
+	if _, err := io.Copy(writer, inFile); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// decrypt encrypted file
+func decryptFile(path, password string) error {
+	salt := []byte(password)
+	key, _ := deriveKey(password, salt)
+
+	inFile, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	var iv [aes.BlockSize]byte
+	stream := cipher.NewOFB(block, iv[:])
+
+	outFile, err := os.OpenFile(path[:len(path)-8], os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	reader := &cipher.StreamReader{S: stream, R: inFile}
+	// Copy the input file to the output file, decrypting as we go.
+	if _, err := io.Copy(outFile, reader); err != nil {
+		return err
+	}
+	return nil
+}
